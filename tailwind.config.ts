ccf7dfb44c6a3443ec646203f4c5dee432138c9e
Isur-Sen9828 <95669455+Isur-Sen9828@users.noreply.github.com>
2024-10-14 23:59:17 +0530

    feat: added shadcn components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,7 +21,7 @@ const config: Config = {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
+  				DEFAULT: '#0D7DFF',
   				foreground: 'hsl(var(--primary-foreground))'
   			},
   			secondary: {
