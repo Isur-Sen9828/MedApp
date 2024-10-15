0294c20d2215ea29aee7dfaca1ef4d3a623d1014
Isur-Sen9828 <95669455+Isur-Sen9828@users.noreply.github.com>
2024-10-15 23:32:42 +0530

    feat: added Header

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,8 +1,38 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import React from 'react'
 
 function Header() {
+  const Menu = [
+    {
+      id:1,
+      name:'Home',
+      path:'/'
+    },
+    {
+      id:2,
+      name:'Explore',
+      path:'/'
+    },
+    {
+      id:3,
+      name:'Contact-Us',
+      path:'/'
+    },
+  ]
   return (
-    <div>Header</div>
+    <div>
+      <div className='flex items-center gap-10'>
+        <Image src='/logo.svg' alt='logo' width={50} height={0}/>
+
+        <ul className='md:flex gap-8 hidden'>
+          {Menu.map((item,index)=>(
+            <li>{item.name}</li>
+          ))}
+        </ul>
+        </div>
+        <Button>Get Started</Button>
+    </div>
   )
 }
 
