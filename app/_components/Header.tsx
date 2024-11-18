e66a8d3d871cd766c0ce33480e39ac880beb1757
Isur-Sen9828 <95669455+Isur-Sen9828@users.noreply.github.com>
2024-11-18 10:55:07 +0530

    feat: added Hero section

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -21,13 +21,13 @@ function Header() {
     },
   ]
   return (
-    <div className='flex justify-between gap-10 items-center shadow-sm'>
-      <div className='flex items-center '>
+    <div className='flex justify-between items-center shadow-sm'>
+      <div className='flex items-center gap-10'>
         <Image src='/logo.svg' alt='logo' width={50} height={0}/>
 
         <ul className='md:flex gap-8 hidden'>
           {Menu.map((item,index)=>(
-            <li>{item.name}</li>
+            <li  className='hover:text-primary cursor-pointer hover:scale-105 transition-all ease-in-out'>{item.name}</li>
           ))}
         </ul>
         </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div>
+        <div className="md:px-20">
           <Header/>
         {children}
         </div>
